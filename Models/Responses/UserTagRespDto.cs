dcd9e8c4fc869c6eaeef3c2d15755bf1f57959dc
chushi2333 <2023048465@qq.com>
2025-03-05 19:18:14 +0800

    用户词库的实体 控制器 仓储的修改

--- Models/Responses/UserTagRespDto.cs
+++ Models/Responses/UserTagRespDto.cs
@@ -1,23 +1,60 @@
 namespace Seiun.Models.Responses;
 
-public class UserTagRespDto
+public class UserUnSelectTagRespDto
 {
-    public Guid UserId { get; set; }
-    public Guid TagId { get; set; }
     public required string TagName { get; set; }
     public required int WordCount { get; set; }
 }
 
-public sealed class UserTagListResp(int code, string message, List<UserTagRespDto>? usertagList)
-    : BaseRespWithData<List<UserTagRespDto>>(code, message, usertagList)
+public sealed class UserUnSelectTagListResp(int code, string message, List<UserUnSelectTagRespDto>? userUnselectedTagList)
+    : BaseRespWithData<List<UserUnSelectTagRespDto>>(code, message, userUnselectedTagList)
 {
-    public static UserTagListResp Success(string message, List<UserTagRespDto> usertagList)
+    public static UserUnSelectTagListResp Success(string message, List<UserUnSelectTagRespDto> userUnselectedTagList)
     {
-        return new UserTagListResp(200, message, usertagList);
+        return new UserUnSelectTagListResp(200, message, userUnselectedTagList);
     }
 
-    public static UserTagListResp Fail(int code, string message)
+    public static UserUnSelectTagListResp Fail(int code, string message)
     {
-        return new UserTagListResp(code, message, null);
+        return new UserUnSelectTagListResp(code, message, null);
     }
+
 }
+
+public class UserSelectedTagRespDto
+{
+    public required string TagName { get; set; }
+    public required int LearnedCount { get; set; }
+    public required int TotalCount { get; set; }
+}
+
+public sealed class UserSelectedTagListResp(int code, string message, List<UserSelectedTagRespDto>? userSelectedTagList)
+    : BaseRespWithData<List<UserSelectedTagRespDto>>(code, message, userSelectedTagList)
+{
+    public static UserSelectedTagListResp Success(string message, List<UserSelectedTagRespDto> userSeletedTagList)
+    {
+        return new UserSelectedTagListResp(200, message, userSeletedTagList);
+    }
+
+    public static UserSelectedTagListResp Fail(int code, string message)
+    {
+        return new UserSelectedTagListResp(code, message, null);
+    }
+
+}
+
+public sealed class SeletedTagDetailResp(int code, string message, UserSelectedTagRespDto? userSelectedTag)
+    : BaseRespWithData<UserSelectedTagRespDto>(code, message, userSelectedTag)
+{
+    public static SeletedTagDetailResp Success(string message, UserSelectedTagRespDto userSelectedTag)
+    {
+        return new SeletedTagDetailResp(200, message, userSelectedTag);
+    }
+
+    public static SeletedTagDetailResp Fail(int code, string message)
+    {
+        return new SeletedTagDetailResp(code, message, null);
+    }
+    
+}
+
--- Repositories/IUserTagRepository.cs
+++ Repositories/IUserTagRepository.cs
@@ -5,6 +5,7 @@ namespace Seiun.Repositories;
 public interface IUserTagRepository : IBaseRepository<UserTagEntity>
 {
     Task<List<TagEntity>> GetUnselectedTagsAsync(Guid userId);
-    Task<List<TagEntity>> GetSelectedTagsAsync(Guid userId);
+    Task<List<UserTagEntity>> GetSelectedTagsAsync(Guid userId);
     Task CancelTagAsync(Guid userId, Guid tagId);
+    Task<UserTagEntity?> GetStudyingTagByUserIdAsync(Guid userId);
 }
